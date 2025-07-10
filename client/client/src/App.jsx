b0db757e1a349d04b5229310f44ae12c003485b8
alsongard <alsongard@gmail.com>
2025-07-11 00:57:14 +0300

    updating packages

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -4,19 +4,33 @@ import Header from './components/header'
 import SignIn from './components/sign_page'
 import PostPage from './components/postPage'
 import Login from './components/login_page'
-function App() {
+import {configureStore} from "@reduxjs/toolkit";
+import reducerer from './store/reducer'
+import {Provider} from "react-redux";
+import requireAuth from './requireAuth.jsx';
 
+const ProtectedPostPage = requireAuth(PostPage);
+const store = configureStore({reducer:reducerer});
+const token = localStorage.getItem('token');
+
+if (token)
+{
+  store.dispatch({type: 'ON_LOGGED_IN'})
+}
+function App() {
   return (
     <>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<div><Header/> <Outlet/></div>}>
-              <Route index element={<SignIn/>}/>
-              <Route path="login" element={<Login/>}/>
-              <Route path="posts" element={<PostPage/>}/>
-            </Route>
-          </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<div><Header/> <Outlet/></div>}>
+                <Route index element={<SignIn/>}/>
+                <Route path="login" element={<Login/> }/>
+                <Route path="posts" element={<ProtectedPostPage/> }/>
+              </Route>
+            </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
--- client/client/src/components/postPage.jsx
+++ client/client/src/components/postPage.jsx
@@ -1,9 +1,18 @@
+import {useState} from "react";
+
 export default function PostPage()
 {
     const username = localStorage.getItem("username");
+    const [postData, setPostData] = useState({});
+
+
     return (
         <section>
             <h1 className="text-black">Welcome back user : {username} </h1>
+
+            <form>
+                
+            </form>
         </section>
     )
 }
