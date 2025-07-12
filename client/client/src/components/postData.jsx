3bd9763fa33d88c45dc49e21dafe1eb43bdc5678
alsongard <alsongard@gmail.com>
2025-07-12 18:59:47 +0300

    postData added

---
+++ client/client/src/components/postData.jsx
@@ -0,0 +1,25 @@
+import doomPlayer from "../assets/images/doomthedarkage.jpg";
+import rocket from "../assets/images/rocket_space.jpg";
+import teslaRbt from "../assets/images/tesla-optimus-gen-2-banner.png"
+
+const postData = [
+    {
+        id: 1,
+        image: rocket, 
+        text: "NASA is targeting July 31 for the launch of its next astronaut mission with SpaceX. The flight, called Crew-11, will lift off atop a Falcon 9 rocket from Launch Complex-39A (LC-39A), at NASA's Kennedy Space Center in Florida, sending the Crew Dragon Endeavour to low Earth orbit (LEO). The flight will mark the sixth mission for Endeavour, making it SpaceX's most-flown Crew Dragon spacecraft.",
+        title: "NASA, SpaceX targeting July 31 "
+    }, 
+    {
+        id: 2,
+        image: teslaRbt, 
+        text: "NASA, SpaceX targeting July 31 ",
+        title: "Tesla"
+    }, 
+    {
+        id: 3,
+        image: doomPlayer, 
+        text: "Doom Eternal built upon the strong foundations that the series' 2016 reboot established, evolving the classic and frenetic first-person action by introducing a complex layer of strategy and quick decision-making. ",
+        title: "Doom The Dark Ages"
+    }
+]
+export default postData;
