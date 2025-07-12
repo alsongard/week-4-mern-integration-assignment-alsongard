ea465125e3c7e37c45ab519af0206a7d18f2f7cc
alsongard <alsongard@gmail.com>
2025-07-12 18:55:08 +0300

    post reader component

---
+++ client/client/src/components/postReaderComponent.jsx
@@ -0,0 +1,14 @@
+function PostReader(props)
+{
+    return (
+        <div className="flex flex-row gap-x-[50px]">
+            <img className="h-[300px] w-[500px] rounded-md" src={props.item.image}/>
+            <div className="flex-[400px_0_0]">
+                <h1>{props.item.title}</h1>
+                <p>{props.item.text}</p>
+            </div>
+        </div>
+    )
+}
+
+export default PostReader;
