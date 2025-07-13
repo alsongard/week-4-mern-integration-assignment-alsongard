24aa2ec3c67868732def17aefb4622cc3f0ccba5
alsongard <alsongard@gmail.com>
2025-07-13 05:33:08 +0300

    added styling for darkmode

--- client/client/src/components/postReaderComponent.jsx
+++ client/client/src/components/postReaderComponent.jsx
@@ -3,7 +3,7 @@ function PostReader(props)
     return (
         <div className="flex flex-row gap-x-[50px]">
             <img className="h-[300px] w-[500px] rounded-md" src={props.item.image}/>
-            <div className="flex-[400px_0_0]">
+            <div className="flex-[400px_0_0] dark:text-white">
                 <h1>{props.item.title}</h1>
                 <p>{props.item.text}</p>
             </div>
