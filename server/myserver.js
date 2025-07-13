94c13e482d01c2a890741a3c2e24bf356139a0fb
alsongard <alsongard@gmail.com>
2025-07-13 23:40:32 +0300

    backend functionality added and all working successfully

--- server/myserver.js
+++ server/myserver.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const cors = require('cors');
 const express = require("express");
 require("dotenv").config();
-
+const Post = require("./models/post.model.js")
 const jwt = require("jsonwebtoken");
 const { default: mongoose } = require("mongoose");
 
@@ -18,11 +18,11 @@ app.get("/", (req,res)=>{
 const saltRounds=10;
 
 app.get("/", (req, res)=>{
-    res.send("<h1>BackEnd API working</h1>")
+    return res.send("<h1>BackEnd API working</h1>")
 })
 // register ==== working successfully
 app.post("/register", async (req,res)=>{
-    console.log(req.body) // testing== integration success: working correctly
+    // console.log(req.body) // testing== integration success: working correctly
     const {useremail, userpassword} = req.body;
     const userEmail = useremail;
     const userPassword  = userpassword
@@ -42,7 +42,7 @@ app.post("/register", async (req,res)=>{
         if (foundEmail)
         {
             // console.log(foundEmail);  testing
-            console.log(`A user has been found with the email: ${userEmail}`);
+            // console.log(`A user has been found with the email: ${userEmail}`);
             return res.status(409).json({success:false, msg:'Email already exist, try another one'});
         }
         // hash the password
@@ -50,7 +50,7 @@ app.post("/register", async (req,res)=>{
         // console.log(`Hash : ${hash}`); testing
         // create user in database
         const user_created = await User.create({email:userEmail, password:hash});
-        console.log(`user_created is :\n ${user_created}`);
+        // console.log(`user_created is :\n ${user_created}`);
         return res.status(200).json({success:true, msg:'Successfully created user'});
     }
     catch(err)
@@ -63,24 +63,24 @@ app.post("/register", async (req,res)=>{
 
 // login ==== working successfully
 app.post("/login", async (req,res)=>{
-    console.log("this is req.body");
-    console.log(req.body);
+    // console.log("this is req.body");
+    // console.log(req.body);
     
     const {useremail, userpassword} = req.body;
     const userEmail = useremail;
     const userPassword = userpassword;
     if (!userEmail || !userPassword)
     {
-        console.log("Email or password undefined");
+        // console.log("Email or password undefined"); testing : working 
         return res.status(400).json({success:false, msg:"Credentials failed!"});
     }
     try
     {
-        console.log(`userEmail: ${userEmail} and userPassword: ${userPassword}`)
+        // console.log(`userEmail: ${userEmail} and userPassword: ${userPassword}`)
         // check if user Exist
         const foundUser = await User.findOne({email:userEmail}); 
         // const foundUser = await User.findById(userId); testing
-        console.log(foundUser); //testing
+        // console.log(foundUser); //testing
         if (!foundUser)
         {
             // return res.status(401).json({success:false, msg:'Invalid credentials! No email or password'}) testing
@@ -108,6 +108,91 @@ app.post("/login", async (req,res)=>{
     }
 })
 
+// working===successfully
+app.post("/createpost", async (req,res)=>{
+    console.log(req.body);
+    const {title, content, excerpt, category, slug, tags, author, published} = req.body;
+    try
+    {
+        if (title)
+        {
+            const user_created =  await Post.create({title:title, slug:slug, content:content, excerpt:excerpt, author:author, tags:tags, isPublished:published})
+            // console.log(user_created);
+            return res.status(200).json({success: true, data:user_created});
+        }
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`)
+        return res.status(400).json({success:false, msg:"Client error! Refresh page and try again"})
+    }
+});
+
+// working===successfully
+app.get("/getpost/:id", async (req, res)=>{
+    const {id}= req.params;
+    // console.log(`id: ${id} and type:${typeof(id)}`)// id:true type:string
+    try
+    {   
+        // const data = await Post.findById(id); // working successfully with post:Object_id('')
+        const data = await Post.find({author:id})
+        // console.log(data);// display on terminal:true
+        return res.status(200).json({success:true, data:data});
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(401).json({success:false, msg:"No post found with the id"})
+    }
+})
+
+// admin link working==successfully
+app.get("/getAllPost", async (req,res)=>{
+    try
+    {
+        const post_data = await Post.find();
+        // console.log(post_data);
+        if (post_data)
+        {
+            return res.status(200).json({success: true, data:post_data});
+        }
+        else
+        {
+            return res.status(500).json({success:false, msg:"Server Error"});
+        }
+    }
+    catch(err)
+    {
+        console.log(`Error ${err}`);
+    }
+})
+
+app.get("/allusers", async (req, res)=>{
+    const user_data = await User.find({}, {email:1, _id: 1});
+    if (user_data)
+    {
+        return res.status(200).json({success:true, data:user_data});
+    }
+    else
+    {
+        return res.status(500).json({success:false, msg:"Server Error"});
+    }
+})
+
+app.delete("post/:id", async (req, res)=>{
+    const {id} = req.params;
+    try
+    {
+        const result = await Post.findByIdAndDelete(id);
+        console.log(result);
+        return res.json(200).json({success:true, data:result});
+    }
+    catch(err)
+    {
+        console.log(`Error : ${err}`);
+    }
+})
+
 const PORT_NUMBER = 5001;
 
 const uri_string = process.env.CONNECT_STRING;
@@ -122,8 +207,3 @@ mongoose.connect(`mongodb+srv://alsongadizo:${secret}@cluster0.9mxopa5.mongodb.n
     })
     .catch((err)=>{console.log(`Error: ${err}`);})
     
-
-
-
-// mongodb+srv://alsongadizo:${secret}@cluster0.9mxopa5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// mongodb+srv://${user}:${secret}@cluster0.9mxopa5.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0
